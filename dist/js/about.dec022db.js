8a9a5aadfb89deca63ff029af0d956de609078a2
JosafatJimenezB <josa221001@gmail.com>
2022-05-15 11:08:47 -0500

    add: añadiendo la carpeta del build

---
+++ dist/js/about.dec022db.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{7381:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var u=e(3396);const a={class:"about"},c=(0,u._)("h1",null,"This is an about page",-1),r=[c];function s(n,t){return(0,u.wg)(),(0,u.iD)("div",a,r)}var i=e(89);const l={},o=(0,i.Z)(l,[["render",s]]);var f=o}}]);
+//# sourceMappingURL=about.dec022db.js.map
